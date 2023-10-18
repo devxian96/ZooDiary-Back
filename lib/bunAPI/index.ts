505566bff90ffbf6eea048a527785cf9d7660368
DevXian <coolman555@me.com>
2023-10-18 21:27:03 +0900

    feat: preflight 옵션 헤더 추가

--- lib/bunAPI/index.ts
+++ lib/bunAPI/index.ts
@@ -94,7 +94,7 @@ const CORS_HEADERS = {
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
 } as const;
 
